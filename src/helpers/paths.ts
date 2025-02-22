d96e87aa50df10de6e83dfe674b8255540494fe3
Dr.Angelicus <Kittyhellokitty555@gmail.com>
2025-02-22 12:56:56 +0300

    jsDoc comments

--- src/helpers/paths.ts
+++ src/helpers/paths.ts
@@ -1,6 +1,10 @@
 import path from "path";
 import { fileURLToPath } from "url";
 
+/**
+ * возвращает файловый путь до директории с html шаблонами
+ * @returns файловый путь до директории с html шаблонами
+ */
 export function template_path() {
     return path.join(path.dirname(fileURLToPath(import.meta.url)), "..", "resources", "templates");
 }
--- src/view/ViewRenderer.ts
+++ src/view/ViewRenderer.ts
@@ -2,24 +2,41 @@ import fs from "fs";
 import { template_path } from "../helpers/paths.js";
 import path from "path";
 import { FileReadError } from "../exceptions/FileReadError.js";
+import { InvalidArgumentError } from "../exceptions/InvalidArgumentError.js";
 
 export class ViewRenderer {
-    async render(templateName: string, variables: Record<string, any>) {
+    /**
+     * рендерит html шаблон
+     * @param {string} templateName имя шаблона без расширения (ожидается, что данный шаблон хранится с расширением .html), пример: шаблон хранится в resources/templates/index.html -> templateName будет index
+     * @param {object} variables все переменные, которые объявляются в шаблоне, обязаны присутствовать в этом объекте, где ключ - название свойства, значение - значение свойства
+     * @returns {Promise} строка, которая содержит html шаблон, в котором плейсхолдеры вида {{ myVar }} заменены на значения одноименных переменных из variables
+     */
+    async render(templateName: string, variables: Record<string, any>): Promise<string> {
         
         const template = await this.readTemplate(templateName);
 
-        const placeholderPattern = /{{\s*[a-zA-Z]+\s*}}/g;
+        const placeholderPattern = /{{\s*[a-zA-Z]*\s*}}/g;
 
         return template.replace(placeholderPattern, (placeholder) => {
-            const variablePattern = /{{\s*([a-zA-Z]+)\s*}}/;
-            const variable = placeholder.match(variablePattern);
+            const variableNamePattern = /{{\s*([a-zA-Z]+)\s*}}/;
+            const variableName = placeholder.match(variableNamePattern);
 
-            if (variable) {
-                return variables[variable[1]];
+            if (variableName) {
+                if (!(variableName[1] in variables)) {
+                    throw new InvalidArgumentError(`variables обязан содержать свойство ${variableName[1]}`);
+                }
+                return variables[variableName[1]];
+            } else {
+                return '';
             }
         });
     }
 
+    /**
+     * читает html шаблон из файла html
+     * @param {string} fileName имя шаблона без расширения (ожидается, что данный шаблон хранится с расширением .html), пример: шаблон хранится в resources/templates/index.html -> fileName будет index
+     * @returns {Promise} html шаблон в виде строки
+     */
     private readTemplate(fileName: string): Promise<string> {
         let pathToFile = path.join(template_path(), fileName + ".html");
 
